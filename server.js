b65d761fbb9fd5a6ae92d4630f0851d7a16196b1
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-05-02 10:12:05 -0700

    small updates to js files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
 const path = require("path");
+const helpers = require('./utils/helpers');
 
 const session = require("express-session");
 const exphbs = require("express-handlebars");
