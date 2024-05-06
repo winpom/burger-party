02b106c0c421285ea8d8809fdb428103e7cfb14f
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-05-06 10:25:40 -0700

    small changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // establishing database connection with provided creds
-const sequelize = new Sequelize(
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
-    dialect: 'postgres'
+    dialect: 'postgres',
   }
 );
 
