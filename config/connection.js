8cbb05918df2991822c1acabdab13f095e41095b
gitongah <mbaabugitonga@gmail.com>
2024-05-06 23:26:08 -0700

    added the port env for the use of diferent ports. changed the review handlebar

--- config/connection.js
+++ config/connection.js
@@ -12,6 +12,8 @@ const sequelize = process.env.DB_URL
   {
     host: 'localhost',
     dialect: 'postgres',
+    //added the db_port for the ones who use a different port number
+    port: process.env.DB_PORT,
   }
 );
 
