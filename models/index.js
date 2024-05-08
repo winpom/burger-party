d3c3d81c9fff9c6e89950f43d327521210219b78
Tyrion Lannister <win.pomerantz@gmail.com>
2024-05-08 10:28:44 -0700

    resolved server errors

--- models/index.js
+++ models/index.js
@@ -3,43 +3,13 @@ const Restaurant = require('./Restaurant');
 const Review = require('./Review');
 const User = require('./User');
 
-<<<<<<< HEAD
-User.hasMany(Review, {
-  foreignKey: '',
-=======
 // Define associations
 User.hasMany(Review, {
   foreignKey: 'user_id',
->>>>>>> 8b6cc99739650d8f4a54a312530581688efa6dba
   onDelete: 'CASCADE'
 });
 
 Review.belongsTo(User, {
-<<<<<<< HEAD
-  foreignKey: ''
-});
-
-Burger.belongsTo(Restaurant, {
-    foreignKey: ''
-  });
-
-Burger.hasMany(Review,{
-  foreignKey:''
-});
-
-Restaurant.hasMany(Burger, {
-  foreignKey: ''
-});
-
-Review.hasOne(Burger,{
-  foreignKey:''
-});
-Restaurant.hasMany(Review,{
-  foreignKey:''
-});
-
-module.exports = { Burger, Restaurant , Review, User };
-=======
   foreignKey: 'user_id'
 });
 
@@ -82,4 +52,4 @@ Review.belongsTo(Restaurant, {
 // });
 
 module.exports = { Burger, Restaurant, Review, User };
->>>>>>> 8b6cc99739650d8f4a54a312530581688efa6dba
+
