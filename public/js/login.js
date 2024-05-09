e764fcc576c370dfd84ea6414ad977dbca7a652f
gitongah <mbaabugitonga@gmail.com>
2024-05-09 03:31:10 -0700

    Added nodemon to be able to run the server continuously. made some improvements on the review cards and the login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,21 @@
+   const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.querySelector("form.login-form");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signup-link a");
+      signupBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -49,3 +67,5 @@ document
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+
+
